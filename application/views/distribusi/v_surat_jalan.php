f46855c781b079a527924932ede682ce13d61569
fhdyt <fhidayat2206@gmail.com>
2021-03-27 10:22:55 +0700

    sfg

--- application/views/distribusi/v_surat_jalan.php
+++ application/views/distribusi/v_surat_jalan.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1 class="m-0">Surat Jalan</h1>
+                    <h1 class="m-0">Surat Jalan & TTBK</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
