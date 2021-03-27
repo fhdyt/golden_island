f6cf281b46b5e114eb76f5e0e8df384c2c99a8d7
fhdyt <fhidayat2206@gmail.com>
2021-03-28 01:17:03 +0700

    mpdf

---
+++ application/views/pdf.php
@@ -0,0 +1,33 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="card card-default color-palette-box">
+        <div class="card-body">
+          <div class="row mb-4">
+            <div class="col-md-12" style="display: flex; align-items: center; justify-content: center;">
+              <h1><?= $title; ?></h1>
+              <img src="<?php echo base_url(); ?>assets/header.png" height="200px">
+            </div>
+          </div>
+
+        </div>
+        <!-- /.card-body -->
+      </div>
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
