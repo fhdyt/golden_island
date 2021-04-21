62cba021ba719423ec75f80c567244cf0355faaa
fhdyt <fhidayat2206@gmail.com>
2021-04-21 14:20:09 +0700

    datatable

--- application/views/_template/footer.php
+++ application/views/_template/footer.php
@@ -92,23 +92,24 @@
 </script>
 
 <script>
-    $(function() {
-        $("#example1").DataTable({
-            "responsive": true,
-            "lengthChange": false,
-            "autoWidth": false,
-            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-        $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-        });
-    });
+    // $(function() {
+    //     $("#example1").DataTable({
+    //         "responsive": true,
+    //         "lengthChange": false,
+    //         "autoWidth": false,
+    //         "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    //     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    //     $('#example2').DataTable({
+    //         "paging": true,
+    //         "lengthChange": false,
+    //         "searching": false,
+    //         "ordering": true,
+    //         "info": true,
+    //         "autoWidth": false,
+    //         "responsive": true,
+    //     });
+    // });
+
     $(".btn_upload").on("click", function() {
         $(".nama_input").val($(this).attr("nama_input"))
         $("#uploadModal").modal("show")
