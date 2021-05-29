d492b95be4379964b3b257d1f470791151f6acfb
fhdyt <fhidayat2206@gmail.com>
2021-05-29 21:29:01 +0700

    favicon

--- application/views/_template/header.php
+++ application/views/_template/header.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>GMS</title>
-  <link href="<?php echo base_url(); ?>assets/theme/favicon.png" rel="icon">
+  <link href="<?php echo base_url(); ?>assets/favicon.png" rel="icon">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
