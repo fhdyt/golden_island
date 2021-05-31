e4fd603597ea63dd7d3d94a7cef375682517864e
fhdyt <fhidayat2206@gmail.com>
2021-05-31 13:13:59 +0700

    tagihan hutang

--- application/views/akuntansi/hutang/v_hutang_riwayat.php
+++ application/views/akuntansi/hutang/v_hutang_riwayat.php
@@ -101,7 +101,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1 class="m-0">Rincian Hutang Relasi</h1>
+                    <h1 class="m-0">Rincian Hutang Supplier</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
