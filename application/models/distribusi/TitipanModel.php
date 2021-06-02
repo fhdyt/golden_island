d3e445c829a756a8b1371b26d2489a7689239d7c
fhdyt <fhidayat2206@gmail.com>
2021-06-02 21:38:19 +0700

    titipan tabung

---
+++ application/models/distribusi/TitipanModel.php
@@ -0,0 +1,73 @@
+<?php
+class TitipanModel extends CI_Model
+{
+
+    public function list()
+    {
+        $hasil = $this->db->query('SELECT * 
+                                    FROM 
+                                    JURNAL_TABUNG AS J
+                                    LEFT JOIN MASTER_RELASI AS R
+                                    ON
+                                    J.MASTER_RELASI_ID=R.MASTER_RELASI_ID
+                                    LEFT JOIN
+                                    MASTER_BARANG AS B
+                                    ON
+                                    J.MASTER_BARANG_ID=B.MASTER_BARANG_ID
+                                    WHERE J.JURNAL_TABUNG_TITIPAN="Yes" 
+                                    AND J.RECORD_STATUS="AKTIF" 
+                                    AND J.PERUSAHAAN_KODE="' . $this->session->userdata('PERUSAHAAN_KODE') . '" 
+                                    AND R.RECORD_STATUS="AKTIF" 
+                                    AND R.PERUSAHAAN_KODE="' . $this->session->userdata('PERUSAHAAN_KODE') . '" 
+                                    AND B.RECORD_STATUS="AKTIF" 
+                                    AND B.PERUSAHAAN_KODE="' . $this->session->userdata('PERUSAHAAN_KODE') . '" 
+                                    ORDER BY J.JURNAL_TABUNG_TANGGAL DESC ')->result();
+        foreach ($hasil as $row) {
+            $row->TANGGAL = tanggal($row->JURNAL_TABUNG_TANGGAL);
+        }
+        return $hasil;
+    }
+
+    public function add()
+    {
+        $data_mp = array(
+            'JURNAL_TABUNG_ID' => create_id(),
+            'MASTER_RELASI_ID' => $this->input->post('relasi'),
+            'MASTER_BARANG_ID' => $this->input->post('jenis'),
+            'JURNAL_TABUNG_TANGGAL' => $this->input->post('tanggal'),
+            'JURNAL_TABUNG_KIRIM' => "0",
+            'JURNAL_TABUNG_KEMBALI' => $this->input->post('jumlah'),
+            'JURNAL_TABUNG_STATUS' => "MP",
+            'JURNAL_TABUNG_KETERANGAN' => $this->input->post('keterangan'),
+            'JURNAL_TABUNG_FILE' => "empty",
+            'JURNAL_TABUNG_TITIPAN' => "Yes",
+
+            'ENTRI_WAKTU' => date("Y-m-d h:i:sa"),
+            'ENTRI_USER' => $this->session->userdata('USER_ID'),
+            'RECORD_STATUS' => "AKTIF",
+            'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+        );
+        $resutl = $this->db->insert('JURNAL_TABUNG', $data_mp);
+        return $resutl;
+    }
+
+    public function hapus($id)
+    {
+        $data = array(
+            'DELETE_WAKTU' => date("Y-m-d h:i:sa"),
+            'DELETE_USER' => $this->session->userdata('USER_ID'),
+            'RECORD_STATUS' => "DELETE",
+            'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+        );
+
+        $this->db->where('JURNAL_TABUNG_ID', $id);
+        $result = $this->db->update('JURNAL_TABUNG', $data);
+        return $result;
+    }
+
+    public function detail($id)
+    {
+        $hasil = $this->db->query('SELECT * FROM PAJAK WHERE PAJAK_ID="' . $id . '" AND RECORD_STATUS="AKTIF" LIMIT 1')->result();
+        return $hasil;
+    }
+}
---
+++ application/views/distribusi/v_titipan.php
@@ -0,0 +1,213 @@
+<div class="modal fade" id="pajakModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah Titipan</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="submit">
+                    <div class="form-group">
+                        <input type="hidden" class="form-control id" name="id" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1"><?= $this->lang->line('tanggal'); ?></label>
+                        <input type="date" class="form-control tanggal" name="tanggal" autocomplete="off" required value="<?= date("Y-m-d"); ?>">
+                        <small class="text-muted">*<?= $this->lang->line('wajib_isi'); ?>.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1"><?= $this->lang->line('Relasi'); ?></label>
+                        <select name="relasi" id="relasi" class="form-control relasi select2" style="width: 100%;" required>
+                            <option value="">-- Pilih Relasi --</option>
+                            <?php foreach (relasi_list() as $row) {
+                            ?>
+                                <option value="<?= $row->MASTER_RELASI_ID; ?>"><?= $row->MASTER_RELASI_NAMA; ?></option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                        <small class="text-muted">*<?= $this->lang->line('wajib_isi'); ?>.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Jenis</label>
+                        <select name="jenis" id="jenis" class="form-control jenis select2" style="width: 100%;" required>
+                            <option value="">-- Jenis --</option>
+
+                            <?php
+                            foreach (tabung() as $row) {
+                            ?>
+                                <option value="<?= $row->MASTER_BARANG_ID; ?>"><?= $row->MASTER_BARANG_NAMA; ?></option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                        <small class="text-muted">*<?= $this->lang->line('wajib_isi'); ?>.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Jumlah</label>
+                        <input type="text" class="form-control jumlah" name="jumlah" autocomplete="off" required value="0">
+                        <small class="text-muted">*<?= $this->lang->line('wajib_isi'); ?>.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Keterangan</label>
+                        <input type="text" class="form-control keterangan" name="keterangan" autocomplete="off">
+                    </div>
+
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><?= $this->lang->line('tutup'); ?></button>
+                <button type="submit" class="btn btn-primary"><?= $this->lang->line('simpan'); ?></button>
+                </form>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1 class="m-0">Titipan Tabung</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="card card-default color-palette-box">
+                <div class="card-body">
+                    <button type="button" class="btn btn-secondary btn_pajak mb-2">Tambah Titipan</button>
+                    <table id="example2" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Tanggal</th>
+                                <th><?= $this->lang->line('nama'); ?></th>
+                                <th>Jenis</th>
+                                <th>Jumlah</th>
+                                <th>Keterangan</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody id="zone_data">
+                            <tr>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<script>
+    $(".btn_pajak").on("click", function() {
+        $("#submit").trigger("reset");
+        $(".id").val("")
+        $("#pajakModal").modal("show")
+    })
+    $(function() {
+        pajak_list();
+    });
+
+    function pajak_list() {
+        $.ajax({
+            type: 'ajax',
+            url: "<?php echo base_url() ?>index.php/distribusi/titipan/list",
+            async: false,
+            dataType: 'json',
+            success: function(data) {
+                $("tbody#zone_data").empty();
+                memuat()
+                console.log(data)
+                if (data.length === 0) {
+                    $("tbody#zone_data").append("<td colspan='10'><?= $this->lang->line('tidak_ada_data'); ?></td>")
+                } else {
+                    var no = 1
+                    for (i = 0; i < data.length; i++) {
+                        $("tbody#zone_data").append("<tr class=''>" +
+                            "<td>" + no++ + ".</td>" +
+                            "<td>" + data[i].TANGGAL + "</td>" +
+                            "<td>" + data[i].MASTER_RELASI_NAMA + "</td>" +
+                            "<td>" + data[i].MASTER_BARANG_NAMA + "</td>" +
+                            "<td>" + data[i].JURNAL_TABUNG_KEMBALI + "</td>" +
+                            "<td>" + data[i].JURNAL_TABUNG_KETERANGAN + "</td>" +
+                            "<td><a class='btn btn-danger btn-sm' onclick='hapus(\"" + data[i].JURNAL_TABUNG_ID + "\")'><i class='fas fa-trash'></i></a></td> " +
+                            "</tr>");
+                    }
+                }
+            },
+            error: function(x, e) {
+                console.log("Gagal")
+            }
+        });
+    }
+
+    $('#submit').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: '<?php echo base_url(); ?>index.php/distribusi/titipan/add',
+            type: "post",
+            data: new FormData(this),
+            processData: false,
+            contentType: false,
+            cache: false,
+            beforeSend: function() {
+                memuat()
+            },
+            success: function(data) {
+                pajak_list();
+                Swal.fire('Berhasil', 'Titipan berhasil ditambahkan', 'success')
+                $("#pajakModal").modal("hide")
+            }
+        });
+    })
+
+    function hapus(id) {
+        console.log(id)
+        Swal.fire({
+            title: '<?= $this->lang->line('hapus'); ?> ?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: `<?= $this->lang->line('hapus'); ?>`,
+            denyButtonText: `Batal`,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: 'ajax',
+                    url: '<?php echo base_url() ?>index.php/distribusi/titipan/hapus/' + id,
+                    beforeSend: function() {
+                        memuat()
+                    },
+                    dataType: 'json',
+                    success: function(data) {
+                        if (data.length === 0) {} else {
+                            pajak_list();
+                            Swal.fire('Berhasil', 'Titipan Berhasil dihapus', 'success')
+                        }
+                    },
+                    error: function(x, e) {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Proses Gagal'
+                        })
+                    } //end error
+                });
+
+            }
+        })
+    }
+</script>
