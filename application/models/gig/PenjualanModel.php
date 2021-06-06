d531431f4c511141fdc198a2e904c0977ca183dd
fhdyt <fhidayat2206@gmail.com>
2021-06-06 13:12:24 +0700

    penjualan

---
+++ application/models/gig/PenjualanModel.php
@@ -0,0 +1,112 @@
+<?php
+class PenjualanModel extends CI_Model
+{
+
+    public function list()
+    {
+        $hasil = $this->db->query('SELECT * 
+                            FROM 
+                            SURAT_JALAN 
+                            WHERE 
+                            SURAT_JALAN_TANGGAL = "' . $this->input->post('tanggal') . '"
+                            AND SURAT_JALAN_JENIS = "penjualan"
+                            AND (SURAT_JALAN_REALISASI_STATUS="selesai" OR SURAT_JALAN_REALISASI_TTBK_STATUS="selesai")
+                            AND RECORD_STATUS="AKTIF" 
+                            AND PERUSAHAAN_KODE="' . $this->input->post('perusahaan') . '" 
+                            ORDER BY SURAT_JALAN_NOMOR ')->result();
+        foreach ($hasil as $row) {
+            $row->RELASI = $this->db->query('SELECT MASTER_RELASI_NAMA FROM MASTER_RELASI WHERE MASTER_RELASI_ID="' . $row->MASTER_RELASI_ID . '" AND RECORD_STATUS="AKTIF" AND PERUSAHAAN_KODE="' . $this->input->post('perusahaan') . '"')->result();
+            $barang = $this->db->query('SELECT * FROM
+                                                        SURAT_JALAN_BARANG AS SJ 
+                                                        LEFT JOIN
+                                                        MASTER_BARANG AS B
+                                                        ON SJ.MASTER_BARANG_ID = B.MASTER_BARANG_ID
+                                                        WHERE SJ.SURAT_JALAN_ID="' . $row->SURAT_JALAN_ID . '"
+                                                        AND SJ.RECORD_STATUS="AKTIF" 
+                                                        AND SJ.PERUSAHAAN_KODE="' . $this->input->post('perusahaan') . '" 
+                                                        AND B.RECORD_STATUS="AKTIF" 
+                                                        AND B.PERUSAHAAN_KODE="' . $this->input->post('perusahaan') . '"')->result();
+            $faktur = $this->db->query('SELECT FAKTUR_ID FROM FAKTUR_SURAT_JALAN WHERE SURAT_JALAN_ID="' . $row->SURAT_JALAN_ID . '" AND RECORD_STATUS="AKTIF" AND PERUSAHAAN_KODE="' . $this->input->post('perusahaan') . '" LIMIT 1')->result();
+            if (empty($faktur)) {
+                foreach ($barang as $row_barang) {
+                    $row_barang->HARGA_BARANG = array();
+                }
+            } else {
+                foreach ($barang as $row_barang) {
+                    $row_barang->HARGA_BARANG = $this->db->query('SELECT * FROM FAKTUR_BARANG
+                                                                    WHERE
+                                                                    FAKTUR_ID="' . $faktur[0]->FAKTUR_ID . '"
+                                                                    AND MASTER_BARANG_ID="' . $row_barang->MASTER_BARANG_ID . '"
+                                                                    AND RECORD_STATUS="AKTIF" 
+                                                                    AND PERUSAHAAN_KODE="' . $this->input->post('perusahaan') . '" LIMIT 1')->result();
+                }
+            }
+
+            $row->BARANG = $barang;
+        }
+        return $hasil;
+    }
+
+    public function add()
+    {
+        if ($this->input->post('id') == "") {
+            $data = array(
+                'PAJAK_ID' => create_id(),
+                'PAJAK_NAMA' => $this->input->post('nama'),
+                'PAJAK_NILAI' => $this->input->post('nilai'),
+
+                'ENTRI_WAKTU' => date("Y-m-d h:i:sa"),
+                'ENTRI_USER' => $this->session->userdata('USER_ID'),
+                'RECORD_STATUS' => "AKTIF",
+                'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+            );
+
+            $result = $this->db->insert('PAJAK', $data);
+            return $result;
+        } else {
+            $data_edit = array(
+                'EDIT_WAKTU' => date("Y-m-d h:i:sa"),
+                'EDIT_USER' => $this->session->userdata('USER_ID'),
+                'RECORD_STATUS' => "EDIT",
+                'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+            );
+
+            $this->db->where('PAJAK_ID', $this->input->post('id'));
+            $edit = $this->db->update('PAJAK', $data_edit);
+
+            $data = array(
+                'PAJAK_ID' => $this->input->post('id'),
+                'PAJAK_NAMA' => $this->input->post('nama'),
+                'PAJAK_NILAI' => $this->input->post('nilai'),
+
+                'ENTRI_WAKTU' => date("Y-m-d h:i:sa"),
+                'ENTRI_USER' => $this->session->userdata('USER_ID'),
+                'RECORD_STATUS' => "AKTIF",
+                'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+            );
+
+            $result = $this->db->insert('PAJAK', $data);
+            return $result;
+        }
+    }
+
+    public function hapus($id)
+    {
+        $data = array(
+            'DELETE_WAKTU' => date("Y-m-d h:i:sa"),
+            'DELETE_USER' => $this->session->userdata('USER_ID'),
+            'RECORD_STATUS' => "DELETE",
+            'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+        );
+
+        $this->db->where('PAJAK_ID', $id);
+        $result = $this->db->update('PAJAK', $data);
+        return $result;
+    }
+
+    public function detail($id)
+    {
+        $hasil = $this->db->query('SELECT * FROM PAJAK WHERE PAJAK_ID="' . $id . '" AND RECORD_STATUS="AKTIF" LIMIT 1')->result();
+        return $hasil;
+    }
+}
---
+++ application/views/gig/v_penjualan.php
@@ -0,0 +1,275 @@
+<div class="modal fade" id="pajakModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah Pajak</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="submit">
+                    <div class="form-group">
+                        <input type="hidden" class="form-control id" name="id" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1"><?= $this->lang->line('nama'); ?></label>
+                        <input type="text" class="form-control nama" name="nama" autocomplete="off">
+                        <small class="text-muted">*<?= $this->lang->line('wajib_isi'); ?>.</small>
+                    </div>
+                    <div class="form-group">
+
+                        <label for="exampleInputEmail1">Nilai</label>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control nilai" name="nilai" autocomplete="off">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">%</span>
+                            </div>
+                        </div>
+                    </div>
+
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><?= $this->lang->line('tutup'); ?></button>
+                <button type="submit" class="btn btn-primary"><?= $this->lang->line('simpan'); ?></button>
+                </form>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1 class="m-0">Penjualan</h1>
+                    <small class="text-muted">PT. GOLDEN ISLAND GROUP</small>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="card card-default color-palette-box">
+                <div class="card-body">
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <select name="perusahaan" id="perusahaan" class="form-control perusahaan select2" style="width: 100%;" required>
+                                <option value="">-- Perusahaan --</option>
+                                <?php
+                                foreach (perusahaan() as $row) {
+                                ?>
+                                    <option value="<?= $row->PERUSAHAAN_KODE; ?>"><?= $row->PERUSAHAAN_NAMA; ?> (<?= $row->PERUSAHAAN_KODE; ?>)</option>
+                                <?php
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="input-group">
+                                <input type="date" class="form-control tanggal" name="tanggal" autocomplete="off" required value="<?= date("Y-m-d"); ?>">
+                                <div class="input-group-append">
+                                    <button class="btn btn-success filter_tanggal"><i class="fas fa-search"></i></button>
+                                </div>
+                            </div>
+                            <small class="text-muted">Tanggal Penjualan.</small>
+                        </div>
+                    </div>
+                    <table id="example2" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th style="vertical-align: middle;">No.</th>
+                                <th style="text-align: center; vertical-align: middle;">Nomor Surat Jalan</th>
+                                <th style="text-align: center; vertical-align: middle;">Nama Relasi</th>
+                                <th style="text-align: center; vertical-align: middle;">Jenis Barang</th>
+                                <th style="text-align: center; vertical-align: middle;">Quantity<br><small class="text-muted">Quantity Isi - Quantity Klaim</small></th>
+                                <th style="text-align: center; vertical-align: middle;">Harga</th>
+                                <th style="text-align: center; vertical-align: middle;">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody id="zone_data">
+                            <tr>
+                            </tr>
+                        </tbody>
+                        <tbody id="zone_data_total">
+                            <tr>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<script>
+    $(".btn_pajak").on("click", function() {
+        $("#submit").trigger("reset");
+        $(".id").val("")
+        $("#pajakModal").modal("show")
+    })
+    $(function() {
+        memuat();
+    });
+
+    function penjualan_list() {
+        $.ajax({
+            type: 'POST',
+            url: "<?php echo base_url() ?>index.php/gig/penjualan/list",
+            async: false,
+            dataType: 'json',
+            data: {
+                tanggal: $(".tanggal").val(),
+                perusahaan: $(".perusahaan").val()
+            },
+            success: function(data) {
+                $("tbody#zone_data").empty();
+                $("tbody#zone_data_total").empty();
+                memuat()
+                console.log(data)
+                if (data.length === 0) {
+                    $("tbody#zone_data").append("<td colspan='10'><?= $this->lang->line('tidak_ada_data'); ?></td>")
+                } else {
+                    var tableContent = "";
+                    var no = 1
+                    var total_seluruh_penjualan = 0
+                    console.log(data.length)
+                    for (i = 0; i < data.length; i++) {
+                        var rowspan = 0;
+                        var detailLength = data[i].BARANG.length;
+                        rowspan += detailLength;
+                        tableContent += "<tr><td rowspan=" + parseInt(1 + rowspan) + ">" + no++ + "</td>" +
+                            "<td rowspan=" + parseInt(1 + rowspan) + ">" + data[i].SURAT_JALAN_NOMOR + "</td>" +
+                            "<td rowspan=" + parseInt(1 + rowspan) + ">" + data[i].RELASI[0].MASTER_RELASI_NAMA + "</td>" +
+                            "</tr>";
+                        var barangLlength = 0;
+
+                        for (var j = 0; j < detailLength; j++) {
+                            total = parseInt(data[i].BARANG[j].SURAT_JALAN_BARANG_QUANTITY) - parseInt(data[i].BARANG[j].SURAT_JALAN_BARANG_QUANTITY_KLAIM)
+                            if (data[i].BARANG[j].HARGA_BARANG.length === 0) {
+                                harga = 0
+                                total_harga = 0
+                            } else {
+                                harga = data[i].BARANG[j].HARGA_BARANG[0].FAKTUR_BARANG_HARGA
+                                total_harga = total * parseInt(data[i].BARANG[j].HARGA_BARANG[0].FAKTUR_BARANG_HARGA)
+                            }
+                            total_seluruh_penjualan += total_harga
+                            tableContent += "<tr>" +
+                                "<td rowspan=" + parseInt(1 + barangLlength) + ">" + data[i].BARANG[j].MASTER_BARANG_NAMA + "<br><small class='text-muted'>" + data[i].BARANG[j].SURAT_JALAN_BARANG_JENIS + "</small></td>" +
+                                "<td rowspan=" + parseInt(1 + barangLlength) + ">" + total + "</td>" +
+                                "<td rowspan=" + parseInt(1 + barangLlength) + ">" + number_format(harga) + "</td>" +
+                                "<td rowspan=" + parseInt(1 + barangLlength) + ">" + number_format(total_harga) + "</td>" +
+                                "</tr>";
+                        }
+
+                    }
+                    $("tbody#zone_data").append(tableContent);
+                    $("tbody#zone_data_total").append("<tr><td colspan='6' style='text-align:right'><b>Total</b></td><td>" + number_format(total_seluruh_penjualan) + "</td></tr>");
+                }
+            },
+            error: function(x, e) {
+                console.log("Gagal")
+            }
+        });
+    }
+
+    $('#submit').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: '<?php echo base_url(); ?>index.php/konfigurasi/pajak/add',
+            type: "post",
+            data: new FormData(this),
+            processData: false,
+            contentType: false,
+            cache: false,
+            beforeSend: function() {
+                memuat()
+            },
+            success: function(data) {
+                pajak_list();
+                Swal.fire('Berhasil', 'Pajak berhasil ditambahkan', 'success')
+                $("#pajakModal").modal("hide")
+            }
+        });
+    })
+
+    function hapus(id) {
+        console.log(id)
+        Swal.fire({
+            title: '<?= $this->lang->line('hapus'); ?> ?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: `<?= $this->lang->line('hapus'); ?>`,
+            denyButtonText: `Batal`,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: 'ajax',
+                    url: '<?php echo base_url() ?>index.php/konfigurasi/pajak/hapus/' + id,
+                    beforeSend: function() {
+                        memuat()
+                    },
+                    dataType: 'json',
+                    success: function(data) {
+                        if (data.length === 0) {} else {
+                            pajak_list();
+                            Swal.fire('Berhasil', 'Pajak Berhasil dihapus', 'success')
+                        }
+                    },
+                    error: function(x, e) {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Proses Gagal'
+                        })
+                    } //end error
+                });
+
+            }
+        })
+    }
+
+    function detail(id) {
+        $.ajax({
+            type: 'ajax',
+            url: '<?php echo base_url() ?>index.php/konfigurasi/pajak/detail/' + id,
+            beforeSend: function() {
+                memuat()
+            },
+            dataType: 'json',
+            success: function(data) {
+                memuat()
+                $(".id").val(data[0].PAJAK_ID)
+                $(".nama").val(data[0].PAJAK_NAMA)
+                $(".nilai").val(data[0].PAJAK_NILAI)
+
+                $("#pajakModal").modal("show")
+            },
+            error: function(x, e) {} //end error
+        });
+    }
+
+    $('.filter_tanggal').on("click", function() {
+        if ($(".perusahaan").val() == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Silahkan pilih Perusahaan terlebih dahulu'
+            })
+        } else {
+            memuat()
+            penjualan_list()
+        }
+
+    });
+</script>
