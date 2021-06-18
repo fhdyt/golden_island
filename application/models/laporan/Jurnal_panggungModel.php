b05168919e855fcbb1fedafc07b6938865d78114
fhdyt <fhidayat2206@gmail.com>
2021-06-18 10:53:36 +0700

    jurnal panggung

---
+++ application/models/laporan/Jurnal_panggungModel.php
@@ -0,0 +1,132 @@
+<?php
+class Jurnal_panggungModel extends CI_Model
+{
+
+    public function list()
+    {
+        if (empty($this->input->post('relasi'))) {
+            $filter_relasi = "";
+        } else {
+            $filter_relasi = 'AND MASTER_RELASI_ID="' . $this->input->post('relasi') . '"';
+        }
+
+        if (empty($this->input->post('tabung'))) {
+            $filter_tabung = "";
+        } else {
+            $filter_tabung = 'AND MASTER_BARANG_ID="' . $this->input->post('tabung') . '"';
+        }
+
+        if (empty($this->input->post('status'))) {
+            $filter_status = "";
+        } else {
+            $filter_status = 'AND PANGGUNG_STATUS_KEPEMILIKAN="' . $this->input->post('status') . '"';
+        }
+
+        if (empty($this->input->post('in_out'))) {
+            $filter_in_out_status = "";
+        } else {
+            $filter_in_out_status = 'AND PANGGUNG_STATUS="' . $this->input->post('in_out') . '"';
+        }
+        if (empty($this->input->post('isi_kosong'))) {
+            $filter_isi_kosong_status = "";
+        } else {
+            $filter_isi_kosong_status = 'AND PANGGUNG_STATUS_ISI="' . $this->input->post('isi_kosong') . '"';
+        }
+
+        $tanggal_dari = $this->input->post("tanggal_dari");
+        $tanggal_sampai = $this->input->post("tanggal_sampai");
+
+        $filter_tanggal = 'AND PANGGUNG_TANGGAL BETWEEN "' . $tanggal_dari . '" AND "' . $tanggal_sampai . '"';
+
+        $hasil = $this->db->query('SELECT * FROM 
+        PANGGUNG 
+        WHERE 
+        RECORD_STATUS="AKTIF" AND PERUSAHAAN_KODE="' . $this->session->userdata('PERUSAHAAN_KODE') . '"
+        ' . $filter_tanggal . '
+        ' . $filter_relasi . '
+        ' . $filter_status . '
+        ' . $filter_tabung . '
+        ' . $filter_in_out_status . '
+        ' . $filter_isi_kosong_status . ' ORDER BY PANGGUNG_KETERANGAN')->result();
+        foreach ($hasil as $row) {
+            $nama_barang = $this->db->query('SELECT MASTER_BARANG_NAMA FROM MASTER_BARANG WHERE MASTER_BARANG_ID="' . $row->MASTER_BARANG_ID . '" AND RECORD_STATUS="AKTIF" AND PERUSAHAAN_KODE="' . $this->session->userdata('PERUSAHAAN_KODE') . '"');
+            $relasi_nama = $this->db->query('SELECT MASTER_RELASI_NAMA FROM MASTER_RELASI WHERE MASTER_RELASI_ID="' . $row->MASTER_RELASI_ID . '" AND RECORD_STATUS="AKTIF" AND PERUSAHAAN_KODE="' . $this->session->userdata('PERUSAHAAN_KODE') . '"');
+            $supplier_nama = $this->db->query('SELECT MASTER_SUPPLIER_NAMA FROM MASTER_SUPPLIER WHERE MASTER_SUPPLIER_ID="' . $row->MASTER_SUPPLIER_ID . '" AND RECORD_STATUS="AKTIF" AND PERUSAHAAN_KODE="' . $this->session->userdata('PERUSAHAAN_KODE') . '"');
+            $row->RELASI_NAMA = $relasi_nama->result();
+            $row->SUPPLIER_NAMA = $supplier_nama->result();
+            $row->NAMA_BARANG = $nama_barang->result();
+            $row->TANGGAL = tanggal($row->PANGGUNG_TANGGAL);
+            // $row->TOTAL = $row->JURNAL_TABUNG_KEMBALI - $row->JURNAL_TABUNG_KIRIM;
+        }
+        return $hasil;
+    }
+
+    public function add()
+    {
+        if ($this->input->post('id') == "") {
+            $data = array(
+                'MASTER_KARYAWAN_ID' => create_id(),
+                'MASTER_KARYAWAN_NAMA' => $this->input->post('nama'),
+                'MASTER_KARYAWAN_JABATAN' => $this->input->post('jabatan'),
+                'MASTER_KARYAWAN_ALAMAT' => $this->input->post('alamat'),
+                'MASTER_KARYAWAN_HP' => $this->input->post('hp'),
+                'MASTER_KARYAWAN_KTP' => $this->input->post('ktp'),
+
+                'ENTRI_WAKTU' => date("Y-m-d G:i:s"),
+                'ENTRI_USER' => $this->session->userdata('USER_ID'),
+                'RECORD_STATUS' => "AKTIF",
+                'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+            );
+
+            $result = $this->db->insert('MASTER_KARYAWAN', $data);
+            return $result;
+        } else {
+            $data_edit = array(
+                'EDIT_WAKTU' => date("Y-m-d G:i:s"),
+                'EDIT_USER' => $this->session->userdata('USER_ID'),
+                'RECORD_STATUS' => "EDIT",
+                'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+            );
+
+            $this->db->where('MASTER_KARYAWAN_ID', $this->input->post('id'));
+            $edit = $this->db->update('MASTER_KARYAWAN', $data_edit);
+
+            $data = array(
+                'MASTER_KARYAWAN_ID' => $this->input->post('id'),
+                'MASTER_KARYAWAN_NAMA' => $this->input->post('nama'),
+                'MASTER_KARYAWAN_JABATAN' => $this->input->post('jabatan'),
+                'MASTER_KARYAWAN_ALAMAT' => $this->input->post('alamat'),
+                'MASTER_KARYAWAN_HP' => $this->input->post('hp'),
+                'MASTER_KARYAWAN_KTP' => $this->input->post('ktp'),
+
+                'ENTRI_WAKTU' => date("Y-m-d G:i:s"),
+                'ENTRI_USER' => $this->session->userdata('USER_ID'),
+                'RECORD_STATUS' => "AKTIF",
+                'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+            );
+
+            $result = $this->db->insert('MASTER_KARYAWAN', $data);
+            return $result;
+        }
+    }
+
+    public function hapus($id)
+    {
+        $data = array(
+            'DELETE_WAKTU' => date("Y-m-d G:i:s"),
+            'DELETE_USER' => $this->session->userdata('USER_ID'),
+            'RECORD_STATUS' => "DELETE",
+            'PERUSAHAAN_KODE' => $this->session->userdata('PERUSAHAAN_KODE'),
+        );
+
+        $this->db->where('MASTER_KARYAWAN_ID', $id);
+        $result = $this->db->update('MASTER_KARYAWAN', $data);
+        return $result;
+    }
+
+    public function detail($id)
+    {
+        $hasil = $this->db->query('SELECT * FROM MASTER_KARYAWAN WHERE MASTER_KARYAWAN_ID="' . $id . '" AND RECORD_STATUS="AKTIF" AND PERUSAHAAN_KODE="' . $this->session->userdata('PERUSAHAAN_KODE') . '" LIMIT 1')->result();
+        return $hasil;
+    }
+}
---
+++ application/views/laporan/v_jurnal_panggung.php
@@ -0,0 +1,228 @@
+<style>
+    .table {
+        font-size: 14px;
+    }
+</style>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1 class="m-0">Jurnal Panggung</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="card card-default color-palette-box">
+                <div class="card-body">
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <select name="relasi_filter" id="relasi_filter" class="form-control relasi_filter select2" style="width: 100%;">
+                                <option value=""><?= $this->lang->line('semua'); ?></option>
+                                <?php
+                                foreach (relasi_list() as $row) {
+                                ?>
+                                    <option value="<?= $row->MASTER_RELASI_ID; ?>"><?= $row->MASTER_RELASI_NAMA; ?></option>
+                                <?php
+                                }
+                                ?>
+                            </select>
+                            <small class="text-muted">Nama Relasi</small>
+                        </div>
+                        <div class="col-md-6">
+                            <select name="tabung_filter" id="tabung_filter" class="form-control tabung_filter select2" style="width: 100%;">
+                                <option value=""><?= $this->lang->line('semua'); ?></option>
+                                <?php
+                                foreach (tabung($relasi[0]->MASTER_RELASI_ID) as $row) {
+                                ?>
+                                    <option value="<?= $row->MASTER_BARANG_ID; ?>"><?= $row->MASTER_BARANG_NAMA; ?></option>
+                                <?php
+                                }
+                                ?>
+                            </select>
+                            <small class="text-muted">Jenis Barang</small>
+                        </div>
+                        <div class="col-md-4 mt-2">
+                            <select name="in_out_filter" id="in_out_filter" class="form-control in_out_filter select2" style="width: 100%;">
+                                <option value=""><?= $this->lang->line('semua'); ?></option>
+                                <option value="in">Masuk</option>
+                                <option value="out">Keluar</option>
+                            </select>
+                            <small class="text-muted">Status Keluar Masuk</small>
+                        </div>
+                        <div class="col-md-4 mt-2">
+                            <select name="isi_kosong_filter" id="isi_kosong_filter" class="form-control isi_kosong_filter select2" style="width: 100%;">
+                                <option value=""><?= $this->lang->line('semua'); ?></option>
+                                <option value="1">Isi</option>
+                                <option value="0">Kosong</option>
+                            </select>
+                            <small class="text-muted">Status Isi Kosong</small>
+                        </div>
+                        <div class="col-md-4 mt-2">
+                            <select name="status_filter" id="status_filter" class="form-control status_filter select2" style="width: 100%;">
+                                <option value=""><?= $this->lang->line('semua'); ?></option>
+                                <option value="MP">MP</option>
+                                <option value="MR">MR</option>
+                            </select>
+                            <small class="text-muted">Status Kepemilikan Tabung</small>
+                        </div>
+                        <div class="col-md-6 mt-2">
+                            <input type="date" class="form-control tanggal_dari" name="tanggal_dari" autocomplete="off" required value="<?= date("Y-m-d"); ?>">
+                            <small class="text-muted">Tanggal Dari.</small>
+                        </div>
+                        <div class="col-md-6 mt-2">
+                            <div class="input-group">
+                                <input type="date" class="form-control tanggal_sampai" name="tanggal_sampai" autocomplete="off" required value="<?= date("Y-m-d"); ?>">
+                                <div class="input-group-append">
+                                    <button class="btn btn-success filter_tanggal"><i class="fas fa-search"></i></button>
+                                </div>
+                            </div>
+                            <small class="text-muted">Tanggal Sampai.</small>
+                        </div>
+                        <div class="col-md-2 mt-4">
+                            <table class="table">
+                                <tr>
+                                    <td class="table-success">Masuk (In)</td>
+                                </tr>
+                                <tr>
+                                    <td class="table-danger">Keluar (Out)</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <table id="example2" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th><?= $this->lang->line('tanggal'); ?></th>
+                                <th><?= $this->lang->line('Relasi'); ?></th>
+                                <th>Supplier</th>
+                                <th>Status</th>
+                                <th>Volume</th>
+                                <th>Masuk</th>
+                                <th>Keluar</th>
+                                <th>Total</th>
+                                <th><?= $this->lang->line('keterangan'); ?></th>
+                            </tr>
+                        </thead>
+                        <tbody id="zone_data">
+                            <tr>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<script>
+    $(function() {
+        jurnal_tabung_list();
+    });
+
+    function jurnal_tabung_list() {
+        $.ajax({
+            type: 'post',
+            url: "<?php echo base_url() ?>index.php/laporan/jurnal_panggung/list",
+            async: false,
+            dataType: 'json',
+            data: {
+                relasi: $(".relasi_filter").val(),
+                tabung: $(".tabung_filter").val(),
+                status: $(".status_filter").val(),
+                tanggal_dari: $('.tanggal_dari').val(),
+                tanggal_sampai: $('.tanggal_sampai').val(),
+                in_out: $(".in_out_filter").val(),
+                isi_kosong: $(".isi_kosong_filter").val(),
+            },
+            success: function(data) {
+                $("tbody#zone_data").empty();
+                memuat()
+                console.log(data)
+                if (data.length === 0) {
+                    $("tbody#zone_data").append("<td colspan='10'><?= $this->lang->line('tidak_ada_data'); ?></td>")
+                } else {
+                    var no = 1
+                    var total = 0
+                    var total_masuk = 0
+                    var total_keluar = 0
+                    for (i = 0; i < data.length; i++) {
+
+                        if (data[i].RELASI_NAMA.length == 0) {
+                            var relasi = "-"
+                        } else {
+                            var relasi = data[i].RELASI_NAMA[0].MASTER_RELASI_NAMA
+                        }
+                        if (data[i].SUPPLIER_NAMA.length == 0) {
+                            var supplier = "-"
+                        } else {
+                            var supplier = data[i].SUPPLIER_NAMA[0].MASTER_SUPPLIER_NAMA
+                        }
+                        if (data[i].PANGGUNG_STATUS == "in") {
+                            var tr = "table-success"
+                            var masuk = data[i].PANGGUNG_JUMLAH
+                            var keluar = 0
+                            total += parseInt(data[i].PANGGUNG_JUMLAH)
+                            total_masuk += parseInt(data[i].PANGGUNG_JUMLAH)
+                        } else {
+                            var tr = "table-danger"
+                            var masuk = 0
+                            var keluar = data[i].PANGGUNG_JUMLAH
+                            total -= parseInt(data[i].PANGGUNG_JUMLAH)
+                            total_keluar += parseInt(data[i].PANGGUNG_JUMLAH)
+                        }
+                        if (data[i].PANGGUNG_STATUS_ISI == "1") {
+                            var volume = "Isi"
+                        } else {
+                            var volume = "Kosong"
+                        }
+                        $("tbody#zone_data").append("<tr class='" + tr + "'>" +
+                            "<td>" + no++ + ".</td>" +
+                            "<td><b>" + data[i].TANGGAL + "</b><br>" + data[i].NAMA_BARANG[0].MASTER_BARANG_NAMA + " (<small>" + data[i].PANGGUNG_STATUS_KEPEMILIKAN + "</small>)</td>" +
+                            "<td>" + relasi + "</td>" +
+                            "<td>" + supplier + "</td>" +
+                            "<td>" + data[i].PANGGUNG_STATUS + "</td>" +
+                            "<td>" + volume + "</td>" +
+                            "<td>" + masuk + "</td>" +
+                            "<td>" + keluar + "</td>" +
+                            "<td>" + total + "</td>" +
+                            "<td>" + data[i].PANGGUNG_KETERANGAN + "</td>" +
+                            "</td>" +
+                            "</tr>");
+                    }
+                    $("tbody#zone_data").append("<tr><td colspan='6' align='right'><b>Total</b></td><td><b>" + total_masuk + "</b></td><td><b>" + total_keluar + "</b></td><td><b>" + (total_masuk - total_keluar) + "</b></td><td><small class='text-muted'></small></td></tr>")
+
+                }
+            },
+            error: function(x, e) {
+                console.log("Gagal")
+            }
+        });
+    }
+
+    $('#tabung_filter,#status_filter, #relasi_filter, #in_out_filter,#isi_kosong_filter').change(function() {
+        memuat()
+        jurnal_tabung_list()
+    });
+
+    $('.filter_tanggal').on("click", function() {
+        memuat()
+        jurnal_tabung_list()
+    });
+
+    // setInterval(function() {
+    //     memuat()
+    //     jurnal_tabung_list()
+    // }, 30000);
+</script>
