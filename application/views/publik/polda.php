3d20690cfa3ce0e1e700a660d6dd9b87738e5b67
fhdyt <fhidayat2206@gmail.com>
2021-07-13 14:37:32 +0700

    laporan polda

--- application/views/publik/polda.php
+++ application/views/publik/polda.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DATA PENYALURAN TABUNG OKSIGEN WILAYAH PROV. BENGKULU</title>
+    <meta property="og:title" content="<?= tanggal($this->uri->segment('3')); ?>">
+    <meta property="og:description" content="Data Penyaluran Tabung Oksigen Wilayah Prov. Bengkulu">
+    <meta property="og:image" content="<?php echo base_url(); ?>uploads/perusahaan/BGS.png">
+    <title>Data Penyaluran Tabung Oksigen Wilayah Prov. Bengkulu</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
